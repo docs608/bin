3b5862bf709747278cf98640dd4e144ad5bb7f94
Shubham Priyadarshi <shubham.priyadarshi@moglix.com>
2019-06-06 11:41:54 +0530

    changes

--- EclipseProjects/Untitled.zip_expanded/DPState_2/src/StatePatternDemo.java
+++ EclipseProjects/Untitled.zip_expanded/DPState_2/src/StatePatternDemo.java
@@ -1,15 +1,17 @@
 
 public class StatePatternDemo {
+
 	public static void main(String[] args) {
-		PlayingState playingState = new PlayingState();
-		MP3PlayerContext mp3PlayerContext = new MP3PlayerContext(playingState);
+		TrafficLight trafficLight = new TrafficLight();
 		while (true) {
-			mp3PlayerContext.play();
+			trafficLight.Reportstate();
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			trafficLight.change();
 		}
 	}
+
 }
