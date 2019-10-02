0126a8704a2d052cc798bffd6c256fef178e1717
Shubham Priyadarshi <shubham.priyadarshi@moglix.com>
2019-10-02 13:11:25 +0530

    changes

--- EclipseProjects/Untitled.zip_expanded/MatrixSpiral/src/sss/matrixspiral/object_oriented/Demo.java
+++ EclipseProjects/Untitled.zip_expanded/MatrixSpiral/src/sss/matrixspiral/object_oriented/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
 				     { 4,  5,  6,  7},
 				     { 8,  9, 10, 11},
 				     {12, 13, 14, 15}};
-		int[]result = new int[16];
+		int[] result;
 		MatrixTraverse matrixTraverse = new MatrixTraverseClockwise();
 		matrixTraverse.setStartPosition(StartPosition.TOP_LEFT);
 		result = matrixTraverse.traverseMatrix(m);
